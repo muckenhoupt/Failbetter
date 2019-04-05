87308fe16b34c9e72c9ce6ae2113e101d84e2205
Henry Senior <henry@failbettergames.com>
2019-04-05 10:18:48 +0100

    added Q2 Materials unity project

---
+++ Q2 Materials/Assets/Plotter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Plotter : MonoBehaviour
+{
+    public GameObject PointPrefab;
+    public GameObject BufferPrefab;
+    public PolygonCollider2D Collider;
+    public float BufferDistance = 40;
+
+    public void Clear()
+    {
+        Debug.Log("Clear");
+    }
+
+    public void PlotPoints()
+    {
+        Debug.Log("PlotPoints");
+    }
+
+    public void PlotBuffer()
+    {
+        Debug.Log("PlotBuffer");
+    }
+}
